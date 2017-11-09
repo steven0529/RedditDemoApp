6989d70df2f4640a836d53f733558656d6d4e1f8
Steven Reyes <sreyes@stratpoint.com>
2017-11-09 19:55:35 +0800

    Add prefix types

---
+++ app/src/main/java/com/steven/redditdemoapp/commons/PrefixType.kt
@@ -0,0 +1,13 @@
+package com.steven.redditdemoapp.commons
+
+/**
+ * Created by Steven Reyes on 09/11/2017
+ */
+object PrefixType {
+    val COMMENTS = "t1"
+    val ACCOUNT = "t2"
+    val LINK = "t3"
+    val MESSAGE = "t4"
+    val SUBREDDIT = "t5"
+    val AWARD = "t6"
+}
