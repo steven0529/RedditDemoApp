16a74027724126ae0e910eff37f147186c35a979
Steven Reyes <sreyes@stratpoint.com>
2017-11-09 18:45:13 +0800

    Added Glide and Okhttp3 dependencies Added RedditNewsApiManager

---
+++ app/src/main/java/com/steven/redditdemoapp/commons/extensions/ImageViewExtension.kt
@@ -0,0 +1,28 @@
+package com.steven.redditdemoapp.commons.extensions
+
+import android.text.TextUtils
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.steven.redditdemoapp.R
+
+/**
+ * Created by Steven Reyes on 09/11/2017
+ */
+
+fun ImageView.loadImg(imageUrl: String) {
+
+    if (TextUtils.isEmpty(imageUrl)) {
+        Glide.with(context)
+                .load(R.mipmap.ic_launcher)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(this)
+    } else {
+        Glide.with(context)
+                .load(imageUrl)
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(this)
+    }
+
+}
---
+++ app/src/main/java/com/steven/redditdemoapp/http/RedditNewsApiManager.kt
@@ -0,0 +1,69 @@
+package com.steven.redditdemoapp.http
+
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import com.steven.redditdemoapp.http.model.NewsBaseResponse
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+/**
+ * Created by Steven Reyes on 09/11/2017
+ */
+object RedditNewsApiManager {
+
+    private const val BASE_URL: String = "https://www.reddit.com/"
+
+    private lateinit var redditApi: RedditApi
+
+    init {
+        val retrofit = initRetrofit()
+        initServices(retrofit)
+    }
+
+    private fun initRetrofit(): Retrofit {
+        val interceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder().apply {
+            networkInterceptors().add(Interceptor { chain ->
+                val original = chain.request()
+                val request = original.newBuilder()
+                        .method(original.method(), original.body())
+                        .build()
+                chain.proceed(request)
+            })
+            addInterceptor(interceptor)
+        }
+
+        return Retrofit.Builder().baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(createMoshiConverter())
+                .client(client.build())
+                .build()
+    }
+
+    private fun createMoshiConverter(): MoshiConverterFactory = MoshiConverterFactory.create()
+
+    private fun initServices(retrofit: Retrofit) {
+        redditApi = retrofit.create(RedditApi::class.java)
+    }
+
+    fun getNews(after: String, limit: String): Observable<NewsBaseResponse> {
+        return redditApi.getTopArticles(after, limit)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())!!
+    }
+
+    fun getComments(subreddit: String, id: String): Observable<NewsBaseResponse> {
+        return redditApi.getComments(subreddit, id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())!!
+    }
+
+}
