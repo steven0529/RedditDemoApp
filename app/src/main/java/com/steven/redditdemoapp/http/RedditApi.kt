7cecff96ead1b8e215931a9549537ea5c3ded2fd
Steven Reyes <sreyes@stratpoint.com>
2017-11-09 18:22:24 +0800

    Added comments api

--- app/src/main/java/com/steven/redditdemoapp/http/RedditApi.kt
+++ app/src/main/java/com/steven/redditdemoapp/http/RedditApi.kt
@@ -3,6 +3,7 @@ package com.steven.redditdemoapp.http
 import com.steven.redditdemoapp.http.model.NewsBaseResponse
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 /**
@@ -13,5 +14,8 @@ interface RedditApi {
 
     @GET("/top.json")
     fun getTopArticles(@Query("after") after: String, @Query("limit") limit: String) : Observable<NewsBaseResponse>
+
+    @GET("/r/{subreddit}/comments/{id}")
+    fun getComments(@Path("subreddit") subreddit: String, @Path("id") id: String) : Observable<NewsBaseResponse>
 
 }
